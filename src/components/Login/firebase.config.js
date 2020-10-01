8fa953f6048313ee0e0d400f45c4bf3a5048d449
mdsanaullahkhan <advsamikhandu@gmail.com>
2020-10-02 01:47:19 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALHLt8tfC4pBz0udEMPRWFPBcrEqbq6S0",
+  authDomain: "burj-al-arab-70f07.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-70f07.firebaseio.com",
+  projectId: "burj-al-arab-70f07",
+  storageBucket: "burj-al-arab-70f07.appspot.com",
+  messagingSenderId: "473374084407",
+  appId: "1:473374084407:web:d276f91c206fa1a1b75eb2"
+};
+  export default firebaseConfig;
